42cd8ab6d4f0f1a7c56628fa8c682eeb48a39675
Tehman Sardar <tehmansardar@gmail.com>
2021-07-23 00:34:02 +0500

    Homescreen design

---
+++ src/screens/Home/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Pressable, ImageBackground, View, Text} from 'react-native';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+
+import styles from './styles';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Pressable
+        style={styles.searchButton}
+        onPress={() => console.warn('Search button clicked')}>
+        <Fontisto name="search" size={25} color={'#f15454'} />
+        <Text style={styles.searchButtonText}>Where are you going?</Text>
+      </Pressable>
+      <ImageBackground
+        source={require('../../../assets/images/wallpaper.jpg')}
+        style={styles.image}>
+        <Text style={styles.title}>Go Near</Text>
+
+        <Pressable
+          style={styles.button}
+          onPress={() => console.warn('Explore button clicked')}>
+          <Text style={styles.buttonText}>Explore nearby stays</Text>
+        </Pressable>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default HomeScreen;
