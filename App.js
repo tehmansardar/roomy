5ddbb29488b274d207cf807a4f87af2cb6a3e5c7
Tehman Sardar <tehmansardar@gmail.com>
2021-07-23 01:40:10 +0500

    Post props

--- App.js
+++ App.js
@@ -9,15 +9,20 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
-import HomeScreen from './src/screens/Home';
+// import HomeScreen from './src/screens/Home';
 import Post from './src/components/Post';
+
+import feed from './assets/data/feed';
+
+const post1 = feed[0];
+const post2 = feed[1];
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View>
         {/* <HomeScreen /> */}
-        <Post />
+        <Post post={post2} />
       </View>
     </SafeAreaView>
   );
