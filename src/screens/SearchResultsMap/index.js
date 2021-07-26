1bad303de3a1f0a2167400ad8c4948897ee41c3c
Tehman Sardar <tehmansardar@gmail.com>
2021-07-26 20:25:41 +0500

    Map and list sync

--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {
   View,
   Text,
@@ -17,11 +17,41 @@ import PostCarouselItem from '../../components/PostCarouselItem';
 const SearchResultsMap = () => {
   const [selectedPlaceId, setSelectPlaceId] = useState(null);
 
+  const flatlist = useRef();
+  const map = useRef();
+
+  const viewConfig = useRef({itemVisiblePercentThreshold: 70});
+  const onViewChanged = useRef(({viewableItems}) => {
+    if (viewableItems.length > 0) {
+      const selectedPlace = viewableItems[0].item;
+      setSelectPlaceId(selectedPlace.id);
+    }
+  });
+
   const width = useWindowDimensions().width;
+
+  useEffect(() => {
+    if (!selectedPlaceId || !flatlist) {
+      return;
+    }
+
+    const index = places.findIndex(place => place.id === selectedPlaceId);
+    flatlist.current.scrollToIndex({index});
+
+    const selectedPlace = places[index];
+    const region = {
+      latitude: selectedPlace.coordinate.latitude,
+      longitude: selectedPlace.coordinate.longitude,
+      latitudeDelta: 0.8,
+      longitudeDelta: 0.8,
+    };
+    map.current.animateToRegion(region);
+  }, [selectedPlaceId]);
 
   return (
     <View style={{width: '100%', height: '100%'}}>
       <MapView
+        ref={map}
         provider={PROVIDER_GOOGLE}
         style={styles.map}
         initialRegion={{
@@ -42,6 +72,7 @@ const SearchResultsMap = () => {
       </MapView>
       <View style={{position: 'absolute', bottom: 40}}>
         <FlatList
+          ref={flatlist}
           data={places}
           renderItem={({item}) => <PostCarouselItem post={item} />}
           horizontal
@@ -49,6 +80,8 @@ const SearchResultsMap = () => {
           snapToInterval={width - 60}
           snapToAlignment={'center'}
           decelerationRate={'fast'}
+          viewabilityConfig={viewConfig.current}
+          onViewableItemsChanged={onViewChanged.current}
         />
       </View>
     </View>
